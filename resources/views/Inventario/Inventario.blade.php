8ec8833377bba46a1905f02365084186023e3d68
Marlen0207 <L202307045@vbravo.tecnm.mx>
2025-04-01 22:52:46 -0600

    Tablas Creadas

---
+++ resources/views/Inventario/Inventario.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tabla Inventario</title>
+    <style>
+        table {
+            width: 80%;
+            border-collapse: collapse;
+            margin: 20px auto;
+        }
+        th, td {
+            border: 1px solid black;
+            padding: 10px;
+            text-align: center;
+        }
+        th {
+            background-color: #f2f2f2;
+        }
+    </style>
+</head>
+<body>
+    <h2>Lista de Personas</h2>
+    <table>
+        <tr>
+            <th>ID_Inventario</th>
+            <th>ID_Producto</th>
+            <th>Cantidad_Disponible</th>
+            <th>Fecha_Actualizacion</th>
+        </tr>
+    </table>
+</body>
+</html>
